be62400f79e5f62603f93c05c6512e069efa6d1b
runjhkim7 <runjhkim@gmail.com>
2017-04-18 17:21:17 +0900

    FILE 작업

--- src/org/test/Filemanager/SubFileTest.java
+++ src/org/test/Filemanager/SubFileTest.java
@@ -5,27 +5,36 @@ import java.io.IOException;
 
 public class SubFileTest {
 
+	public static void getFileList(String path) {
+		File dir = new File(path);
+
+		File[] fileList = dir.listFiles();
+
+		for (int i = 0; i < fileList.length; i++) {
+
+			File file = fileList[i];
+
+			if (file.isFile()) {
+				// if(file.getName().contains("ini")) {
+
+				// 파일이 있다면 파일 이름 출력
+				System.out.println("\t 파일 이름 = " + file.getName());
+
+			}
+		}
+
+	}
+
 	public static void main(String[] args) {
+		String path = "c:\\";
+		System.out.println("### " + path + " result");
 		// TODO Auto-generated method stub
-		File dir = new File("c:\\"); 
+		getFileList(path);
 
-        File[] fileList = dir.listFiles(); 
-
-        for(int i = 0 ; i < fileList.length ; i++){
-
-		    File file = fileList[i]; 
-
-		    if(file.isFile()){
-		      if(file.getName().contains("ini")) {
-
-	   // 파일이 있다면 파일 이름 출력
-			        System.out.println("\t 파일 이름 = " + file.getName());
-	
-		  	      }
-		      }
-		  }
-
-		
+		path = "c:\\canon";
+		System.out.println("### " + path + " result");
+		// TODO Auto-generated method stub
+		getFileList(path);
 	}
 
 }
